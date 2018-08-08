b196e37ee6285c3359300869adc4e0d441c41744
Israel Bernal <ibernal@tritansoftware.com>
2018-08-08 11:21:36 -0400

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { ServiceWorkerModule } from '@angular/service-worker';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +9,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ServiceWorkerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
